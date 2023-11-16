028c4142c1d9375eca10d32892e97af0023225fd
collinBlessing <nahuriracollinblessing@gmail.com>
2023-11-16 21:16:25 +0300

    replaced with graph

--- app/src/main/java/com/example/socialmediamonitor/MainActivity.java
+++ app/src/main/java/com/example/socialmediamonitor/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.socialmediamonitor;
 
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -8,6 +12,9 @@ import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.Toast;
+import android.widget.Toolbar;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,7 +26,9 @@ public class MainActivity extends AppCompatActivity {
 
     //connect button
     Button connectButton;
-
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,6 +42,11 @@ public class MainActivity extends AppCompatActivity {
         adapter_items = new ArrayAdapter<>(this, R.layout.list_item, months_worked_on);
         autoCompleteTextView.setAdapter(adapter_items);
 
+            //drawer layout
+        drawerLayout = findViewById(R.id.drawer_layout);
+
+
+        //set
         autoCompleteTextView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
